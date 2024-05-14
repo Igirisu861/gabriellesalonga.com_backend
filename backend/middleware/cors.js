d128d099aac59769aeaab9dcdb71776fafa8ac30
Igirisu861 <148285235+Igirisu861@users.noreply.github.com>
2024-05-14 16:17:43 -0600

    Delete backend/middleware/cors.js

--- backend/middleware/cors.js
+++
@@ -1,9 +0,0 @@
-const cors = require('cors');
-
-const corsOptions = {
-    origin: "http://127.0.0.1:5500",
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-};
-
-module.exports = cors(corsOptions);
