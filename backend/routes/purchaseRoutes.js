da909448112787d4998a2344bee959f833c464d9
Igirisu861 <d.r.g.1835@gmail.com>
2024-05-14 12:54:54 -0600

    working code

--- backend/routes/purchaseRoutes.js
+++ backend/routes/purchaseRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const {getPurchases,addArticles,crearPurchases,deletePurchases, deleteArticle} = require('../controllers/purchasesController');
+const {getPurchases,addArticles,crearPurchases,deletePurchases} = require('../controllers/purchasesController');
 
 router.route('/').get(getPurchases).post(crearPurchases);
-router.route('/:id').put(addArticles).delete(deletePurchases).patch(deleteArticle);
+router.route('/:id').put(addArticles).delete(deletePurchases)
 
 module.exports = router
